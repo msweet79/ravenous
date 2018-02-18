50a1ffce8fc7b5310e013f567c01d4064b88ba93
Mike Sweet <sweetmichael_j@yahoo.com>
2018-02-18 11:45:52 -0500

    Moved app files to src/Components/App folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+//Default generic code when created
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
     );
   }
 }
