ca3b6c095ee34acad1af63bab50d34c56f0459c7
Mike Sweet <sweetmichael_j@yahoo.com>
2018-02-22 19:21:43 -0500

    Upload of parts II and III created off line. SearchBar line 63 not working

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,39 +1,87 @@
 import React from 'react';
-import '/SearchBar.css';
+import './SearchBar.css';
 
+class SearchBar extends React.Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    term: '',
+    location: '',
+    sortBy: 'best_match'
+  };
+
+this.handleTermChange = this.handleTermChange.bind(this);
+this.handleLocationChange = this.handleLocationChange.bind(this);
+this.handleSearch = this.handleSearch.bind(this);
+
+//Block of code moved from line 4 per Slack discussion
 const sortByOptions = {
-  //keys come from the Yelp open source
   'Best Match': 'best_match',
   'Highest Rated': 'rating',
-  'Most Reviewd': 'review_count'
+  'Most Reviewed': 'review_count'
 };
+//Constructor close out }
+};
+//end of constructor
 
-class SearchBar extentds React.Component {
+getSortByClass(sortByOption) {
+  if (this.state.sortBy === sortByOption) {
+    return 'active';
+  } else {
+    return '';
+  };
+}
+
+handleSortByChange(sortByOption) {
+  this.setState({sortBy: sortByOption});
+}
+
+handleTermChange(event) {
+  this.setState({term: event.target.value});
+}
+
+handleLocationChange(event) {
+  this.setState({location: event.target.value});
+}
+
+handleSearch(event) {
+  this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+//Given from the instructions
+  event.preventDefault()
+}
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li {key=sortByOptionValue}>{sortByOption}</li>;
+//changed Object.keys(sortByOptions) to Object.keys(this.sortByOptions) per slack discussion
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+      key={sortByOptionValue}
+      //Not sure if I did step 9 correct
+      onClick={this.handleSortByChange.bind(this, sortByOptionValue)>
+      {sortByOption}
+    </li>;
     });
   }
 
   render() {
-    return(
+    return (
       <div className="SearchBar">
-  <div className="SearchBar-sort-options">
-    <ul>
-      {this.renderSortByOptions()};
-    </ul>
-  </div>
-  <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
-  </div>
-  <div className="SearchBar-submit">
-  //getting an error with the '.  Trying the HTML code for it.
-    <a>Let&#39;s Go</a>
-  </div>
-</div>
-);
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
+        </div>
+        <div className="SearchBar-submit">
+        //getting an error with the '.  Trying the HTML code for it.
+          <a onClick={this.handleSearch}>Let&#39;s Go</a>
+        </div>
+      </div>
+    );
   }
 }
 
